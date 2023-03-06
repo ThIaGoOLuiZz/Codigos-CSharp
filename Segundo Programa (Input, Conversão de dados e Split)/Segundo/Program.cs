d3f8cdeae6e9ec44f0db172ca26ac7afe9f4ed7b
ThIaGoOLuiZz <126069763+ThIaGoOLuiZz@users.noreply.github.com>
2023-03-06 17:21:37 -0300

    Add files via upload
    Segundo Programa

---
+++ Segundo Programa (Input, Conversão de dados e Split)/Segundo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Segundo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+        //INPUT PARA STRING
+        Console.WriteLine("Entre com seu nome completo:");
+        string nome = Console.ReadLine();
+
+        //INPUT PARA NUMERO INTEIRO
+        Console.WriteLine("Quantos quartos tem na sua casa?");
+        int quartos = int.Parse(Console.ReadLine());
+
+        //INPUT PARA NUMERO DOUBLE
+        Console.WriteLine("Entre com o preço de um produto:");
+        double preco = double.Parse(Console.ReadLine());
+
+        //INPUT PARA SPLIT DE VALORES
+        Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+        string[] vetor = (Console.ReadLine().Split(' '));
+
+        Console.WriteLine(nome);
+        Console.WriteLine(quartos);
+        Console.WriteLine(preco.ToString("F2"));
+        Console.WriteLine(vetor[0]);
+        Console.WriteLine(int.Parse(vetor[1]));
+        Console.WriteLine(double.Parse(vetor[2]). ToString("F2"));
+
+
+
+
+
+
+
+
+    }
+}
